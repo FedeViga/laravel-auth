e269b4fa7c7344bb8779a91732e487f90bade060
federico_viganò <fede_viga@hotmail.it>
2024-04-18 16:46:27 +0200

    completata milestone 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
 <div class="container py-5">
-    <h1>
-        Dashboard
-    </h1>
+    <h2 class="fs-4 text-secondary my-4">
+        {{ __('Dashboard') }}
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">{{$user->name}}'s {{ __(' Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
