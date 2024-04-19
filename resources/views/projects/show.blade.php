cc1bb6359c244faa39ce67b985d2a6e6fa223c90
federico_viganò <fede_viga@hotmail.it>
2024-04-19 12:53:06 +0200

    tabelle show responsive

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -9,27 +9,58 @@
         </h2>
 
         <div class="px-3">
-
-            <table class="table w-auto">
-                <tbody>
-                  <tr>
-                    <th scope="row">Description:</th>
-                    <td>{{$project->description}}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Thumbnail:</th>
-                    <td><a class="text-white" href="#">{{$project->thumb}}</a></td>
-                  </tr>
-                  <tr>
-                    <th class="pe-3" scope="row">Technologies:</th>
-                    <td>{{$project->technologies}}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Github Link:</th>
-                    <td><a class="text-white" href="#">{{$project->link}}</a></td>
-                  </tr>
-                </tbody>
-            </table>
+                <table class="table w-auto">
+                    <tbody>
+                      <tr>
+                        <th scope="row" class="d-none d-sm-table-cell">
+                            Description:
+                        </th>
+                        <td>
+                            <h5 class="d-block d-sm-none">
+                                Description:
+                            </h5>
+                            {{$project->description}}
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row" class="d-none d-sm-table-cell">
+                            Thumbnail:
+                        </th>
+                        <td>
+                            <h5 class="d-block d-sm-none">
+                                Thumbnail:
+                            </h5>
+                            <a class="text-white text-break" href="#">
+                                {{$project->thumb}}
+                            </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row" class="d-none d-sm-table-cell">
+                            Technologies:
+                        </th>
+                        <td>
+                            <h5 class="d-block d-sm-none">
+                                Technologies:
+                            </h5>
+                            {{$project->technologies}}
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row" class="d-none d-sm-table-cell">
+                            Github Link:
+                        </th>
+                        <td>
+                            <h5 class="d-block d-sm-none">
+                                GitHub Links:
+                            </h5>
+                            <a class="text-white text-break" href="#">
+                                {{$project->link}}
+                            </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                </table>
 
             <a href="{{route('projects.edit', $project->id)}}" class="btn btn-outline-warning">Edit</a>
             
