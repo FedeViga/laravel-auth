171e9a66d2d1816552220b01eb4020a94286a6cc
federico_viganò <fede_viga@hotmail.it>
2024-04-19 15:03:54 +0200

    aggiunta delete con modale per la conferma

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -63,6 +63,34 @@
                 </table>
 
             <a href="{{route('projects.edit', $project->id)}}" class="btn btn-outline-warning">Edit</a>
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                Delete
+            </button>
+
+            <!-- Modal -->
+            <div class="modal" tabindex="-1" id="deleteModal">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title">Delete Project</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <p>Are you sure you want to delete {{$project->title}}?</p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <form action="{{route('projects.destroy', $project->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger">Delete</button>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             
         </div>
 
